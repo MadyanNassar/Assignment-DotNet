1989b66927d08341ec6565fd0ce96b33a52c16ed
MadyanNassar <76964256+MadyanNassar@users.noreply.github.com>
2023-02-13 03:53:56 +0100

    Add second question

---
+++ SecondApp/SecondApp/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SecondApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var intArray = new ValuesHandler();
+            double sum = intArray.GetSum();
+
+            return Ok(sum);
+
+        }
+    }
+}
---
+++ SecondApp/SecondApp/ValuesHandler.cs
@@ -0,0 +1,23 @@
+﻿
+namespace SecondApp
+{
+    public class ValuesHandler
+    {
+        public double GetSum()
+        {
+
+            string[] stringArr = { "42", "1e3", "1.222", null, "-12", "test" };
+
+            var newStringArr = stringArr
+                               .Where(c => c != null && double.TryParse(c, out var val) == true)
+                               .Select(x => x.Replace(".", string.Empty))
+                               .ToArray();
+
+            double[] doubleArr = Array.ConvertAll(newStringArr, s => double.Parse(s));
+
+
+            return doubleArr.Sum();
+        }
+
+    }
+}
